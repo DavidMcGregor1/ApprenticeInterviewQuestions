4ae1eb48bbd8868b5c5283d87cd3e92cb4d5a675
David McGregor <david.mcgregor@hl.co.uk>
2024-03-11 16:04:13 +0000

    adding more questions

--- script.js
+++ script.js
@@ -1,24 +1,68 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const questions = [
-    "Tell me about yourself.",
-    "What are your strengths and weaknesses?",
-    "Why do you want to work for this company?",
-    "Can you describe a difficult work situation and how you overcame it?",
-    "Where do you see yourself in five years?",
-    "Why should we hire you?",
-    "What do you know about our company?",
-    "Do you have any questions for us?",
-    "Tell me about a time when you had to work with a difficult coworker or manager.",
-    "What motivates you?",
-    "How do you handle stress and pressure?",
-    "Describe a time when you failed and how you handled it.",
-    "What are your salary expectations?",
-    "How do you stay organized and manage your time?",
-    "What do you enjoy doing outside of work?",
-    "Describe your ideal work environment."
+  let questions = [];
+  let usedQuestions = [];
+
+  const portfolioQuestions = [
+    "Talk to me about your company's approach to version / source control",
+    "What are some of the roles and responsibilities within the SDLC",
+    "You've mentioned you used a relational database. Can you talk to me about a non-relational database?",
+    "How have you linked code to data sets?",
+    "What are some of the organisational poliwcies to do with sensitive data?",
+    "Can you give an example of how you have been committed to personal development?",
+    "How does testing usually happen within your company?",
+    "What is your company's apporach to deployment?",
+    "What is your company's view / position on cloud services?",
+    "What are some security policies in place in your company?",
+    "You've mentioned CI/CD, can you give some advantages of using this?",
+    "What are some of the responsibilities within your squad?",
+    "What development methodology is used in your company, how does it differ from another methodology?",
+    "Can you explain where you would use a non-relational database over a relational datebase?",
+    "What are some skills you've learnt during your apprenticeship?",
+    "Can you give an example of a time you created an analysis artefact?",
+    "Can you give an example of a time you developed an effective user interface?",
+    "Can you give an example of a time you implemented a design while remaining compliant with security requirements?",
+    "What are your experiences with working independently vs collaboratively?",
+    "Can you give some examples of when you have communicated effectively?",
   ];
 
-  let usedQuestions = [];
+  const projectQuestions = [
+    "Can you describe some of the roles and resposibilities in the software development lifecycle and how they contribute to a project?",
+    "Can you explain some ways that a development team could work effectively together?",
+    "Can you provide an example of where you applied algorithms, logic and datastructures to solve a problem?",
+    "How did you ensure the software development apporach you adopted was justified based on the functional specifications you provided?",
+    "Can you provide an example of a situation where you had to make a decision regarding software design and development approach? How did you justify your choice based on the specifications?",
+    "Can you explain the approach you took to create logical and maintainable code for this project?",
+    "How did you ensure that your code was easy to understand and maintain by other developers?",
+    "What considerations did you take into account when making decisions about the tools, frameworks, or design patterns to use in your code?",
+    "Can you describe a situation where you analyzed unit testing results, found errors, and then corrected them? What was your process?",
+    "Can you explain to me how you approach reviewing unit testing results to identify issues? How would you ensure those corrections are successfully implemented?",
+    "How did you go about identifying the test scenarios for this project?",
+    "Can you provide an example of a test scenario you created and explain how it satisfied the project specification?",
+    "Can you describe a situation in which you encountered a problem or flaw in a code during a work-based project? How did you approach the issue and what structured techniques did you apply to identify and resolve the problem?",
+    "Sometimes, coding errors can be difficult to detect and resolve. Can you share an experience in which you faced a particularly challenging coding issue? How did you apply structured problem-solving techniques to identify the root cause and successfully debug the flaw in the code?",
+    "In your experience, what are the key considerations when reviewing your own contribution to code building, managing and deploying? How do you ensure these considerations align with the project specification?",
+    "Can you walk me through the logical thinking process you followed while designing and implementing this software feature? How did you ensure that your decisions and reasoning were valid? How did you approach debugging and troubleshooting issues in the codebase?",
+    "Can you provide an example where you used valid reasoning to identify and fix a bug? ",
+    "Tell me about a time when you had to make a justified decision about the choice of technology, framework, or architecture while working on a project",
+    "How did you prioritize maintaining a productive working environment throughout the project? Can you provide examples of specific actions you took to encourage productivity and ensure a positive team dynamic?",
+    "In terms of maintaining a secure working environment, can you discuss any steps you took to protect sensitive information or ensure data privacy during the project? How did you manage potential security risks or vulnerabilities within the software or infrastructure?",
+  ];
+
+  function toggleToPortfolioQuestions() {
+    questions = portfolioQuestions.slice();
+    usedQuestions = [];
+    displayRandomQuestion();
+    document.getElementById('portfolio-button').classList.add('selected');
+    document.getElementById('project-button').classList.remove('selected');
+  }
+
+  function toggleToProjectQuestions() {
+    questions = projectQuestions.slice();
+    usedQuestions = [];
+    displayRandomQuestion();
+    document.getElementById('project-button').classList.add('selected');
+    document.getElementById('portfolio-button').classList.remove('selected');
+  }
 
   function displayRandomQuestion() {
     if (questions.length === 0) {
@@ -33,7 +77,24 @@ document.addEventListener('DOMContentLoaded', function() {
     questions.splice(randomIndex, 1);
   }
 
-  document.getElementById('show-definition-button').addEventListener('click', displayRandomQuestion);
+  const nextButton = document.getElementById('next-question-button')
+  nextButton.addEventListener('click', () => {
+    displayRandomQuestion();
+  });
+  const portfolioButton = document.getElementById('portfolio-button')
+  portfolioButton.addEventListener('click', () => {
+    portfolioButton.classList.add("clicked")
+    projectButton.classList.remove("clicked")
+    toggleToPortfolioQuestions();
+  })
+  
+  const projectButton = document.getElementById('project-button')
+  projectButton.addEventListener('click', () => {
+    projectButton.classList.add("clicked")
+    portfolioButton.classList.remove("clicked")
+    toggleToProjectQuestions();
+  });
 
-  displayRandomQuestion();
+  // Default to JavaScript questions
+  portfolioButton.click();
 });
